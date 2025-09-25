d50a0a320f38d0058c0cc25ba99a7e4ff0dc4d58
Nguyen The Bach <178692316+djgmh9@users.noreply.github.com>
2025-09-25 18:14:01 +0800

    Fix wrongly refactored Address to Remark in test files

--- src/test/java/seedu/address/model/person/AddressTest.java
+++ src/test/java/seedu/address/model/person/AddressTest.java
@@ -10,36 +10,36 @@ public class AddressTest {
 
     @Test
     public void constructor_null_throwsNullPointerException() {
-        assertThrows(NullPointerException.class, () -> new Remark(null));
+        assertThrows(NullPointerException.class, () -> new Address(null));
     }
 
     @Test
     public void constructor_invalidAddress_throwsIllegalArgumentException() {
         String invalidAddress = "";
-        assertThrows(IllegalArgumentException.class, () -> new Remark(invalidAddress));
+        assertThrows(IllegalArgumentException.class, () -> new Address(invalidAddress));
     }
 
     @Test
     public void isValidAddress() {
         // null address
-        assertThrows(NullPointerException.class, () -> Remark.isValidAddress(null));
+        assertThrows(NullPointerException.class, () -> Address.isValidAddress(null));
 
         // invalid addresses
-        assertFalse(Remark.isValidAddress("")); // empty string
-        assertFalse(Remark.isValidAddress(" ")); // spaces only
+        assertFalse(Address.isValidAddress("")); // empty string
+        assertFalse(Address.isValidAddress(" ")); // spaces only
 
         // valid addresses
-        assertTrue(Remark.isValidAddress("Blk 456, Den Road, #01-355"));
-        assertTrue(Remark.isValidAddress("-")); // one character
-        assertTrue(Remark.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
+        assertTrue(Address.isValidAddress("Blk 456, Den Road, #01-355"));
+        assertTrue(Address.isValidAddress("-")); // one character
+        assertTrue(Address.isValidAddress("Leng Inc; 1234 Market St; San Francisco CA 2349879; USA")); // long address
     }
 
     @Test
     public void equals() {
-        Remark address = new Remark("Valid Address");
+        Address address = new Address("Valid Address");
 
         // same values -> returns true
-        assertTrue(address.equals(new Remark("Valid Address")));
+        assertTrue(address.equals(new Address("Valid Address")));
 
         // same object -> returns true
         assertTrue(address.equals(address));
@@ -51,6 +51,6 @@ public class AddressTest {
         assertFalse(address.equals(5.0f));
 
         // different values -> returns false
-        assertFalse(address.equals(new Remark("Other Valid Address")));
+        assertFalse(address.equals(new Address("Other Valid Address")));
     }
 }
--- src/test/java/seedu/address/testutil/PersonBuilder.java
+++ src/test/java/seedu/address/testutil/PersonBuilder.java
@@ -3,7 +3,7 @@ package seedu.address.testutil;
 import java.util.HashSet;
 import java.util.Set;
 
-import seedu.address.model.person.Remark;
+import seedu.address.model.person.Address;
 import seedu.address.model.person.Email;
 import seedu.address.model.person.Name;
 import seedu.address.model.person.Person;
@@ -24,7 +24,7 @@ public class PersonBuilder {
     private Name name;
     private Phone phone;
     private Email email;
-    private Remark address;
+    private Address address;
     private Set<Tag> tags;
 
     /**
@@ -34,7 +34,7 @@ public class PersonBuilder {
         name = new Name(DEFAULT_NAME);
         phone = new Phone(DEFAULT_PHONE);
         email = new Email(DEFAULT_EMAIL);
-        address = new Remark(DEFAULT_ADDRESS);
+        address = new Address(DEFAULT_ADDRESS);
         tags = new HashSet<>();
     }
 
@@ -69,7 +69,7 @@ public class PersonBuilder {
      * Sets the {@code Address} of the {@code Person} that we are building.
      */
     public PersonBuilder withAddress(String address) {
-        this.address = new Remark(address);
+        this.address = new Address(address);
         return this;
     }
 
