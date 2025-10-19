fff40e4d6b43980cd05c25e04facebcab3554be3
Hai <hoanghai050605@gmail.com>
2025-10-19 23:19:07 +0800

    Fix checkstyle

--- src/main/java/seedu/address/logic/parser/AddToCommandParser.java
+++ src/main/java/seedu/address/logic/parser/AddToCommandParser.java
@@ -4,7 +4,6 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_CLUB;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_MEMBER;
-import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 
 import java.util.stream.Stream;
 
